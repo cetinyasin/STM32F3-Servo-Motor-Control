447462a0d132deb48de60f53a94e147e68620db4
Yasin Cetin <yasincetin1@gmail.com>
2016-10-25 09:31:01 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,340 @@
+﻿namespace Test_SerialPort
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.cb_Ports = new System.Windows.Forms.ComboBox();
+            this.cb_BRates = new System.Windows.Forms.ComboBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_Open = new System.Windows.Forms.Button();
+            this.btn_Close = new System.Windows.Forms.Button();
+            this.tb_Send = new System.Windows.Forms.TextBox();
+            this.btn_Send = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lb_Received = new System.Windows.Forms.ListBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.track_label = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btn_Temizle = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.gB_pos = new System.Windows.Forms.GroupBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            this.gB_pos.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // cb_Ports
+            // 
+            this.cb_Ports.FormattingEnabled = true;
+            this.cb_Ports.Location = new System.Drawing.Point(19, 33);
+            this.cb_Ports.Name = "cb_Ports";
+            this.cb_Ports.Size = new System.Drawing.Size(103, 21);
+            this.cb_Ports.TabIndex = 0;
+            // 
+            // cb_BRates
+            // 
+            this.cb_BRates.FormattingEnabled = true;
+            this.cb_BRates.Location = new System.Drawing.Point(143, 33);
+            this.cb_BRates.Name = "cb_BRates";
+            this.cb_BRates.Size = new System.Drawing.Size(103, 21);
+            this.cb_BRates.TabIndex = 0;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(19, 60);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(227, 23);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Ports";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(140, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Baud Rate";
+            // 
+            // btn_Open
+            // 
+            this.btn_Open.Location = new System.Drawing.Point(19, 89);
+            this.btn_Open.Name = "btn_Open";
+            this.btn_Open.Size = new System.Drawing.Size(103, 35);
+            this.btn_Open.TabIndex = 3;
+            this.btn_Open.Text = "Open ";
+            this.btn_Open.UseVisualStyleBackColor = true;
+            this.btn_Open.Click += new System.EventHandler(this.btn_Open_Click);
+            // 
+            // btn_Close
+            // 
+            this.btn_Close.Location = new System.Drawing.Point(143, 89);
+            this.btn_Close.Name = "btn_Close";
+            this.btn_Close.Size = new System.Drawing.Size(103, 35);
+            this.btn_Close.TabIndex = 3;
+            this.btn_Close.Text = "Close";
+            this.btn_Close.UseVisualStyleBackColor = true;
+            this.btn_Close.Click += new System.EventHandler(this.btn_Close_Click);
+            // 
+            // tb_Send
+            // 
+            this.tb_Send.Location = new System.Drawing.Point(16, 19);
+            this.tb_Send.Multiline = true;
+            this.tb_Send.Name = "tb_Send";
+            this.tb_Send.Size = new System.Drawing.Size(60, 22);
+            this.tb_Send.TabIndex = 4;
+            // 
+            // btn_Send
+            // 
+            this.btn_Send.Location = new System.Drawing.Point(6, 47);
+            this.btn_Send.Name = "btn_Send";
+            this.btn_Send.Size = new System.Drawing.Size(82, 26);
+            this.btn_Send.TabIndex = 5;
+            this.btn_Send.Text = "Send";
+            this.btn_Send.UseVisualStyleBackColor = true;
+            this.btn_Send.Click += new System.EventHandler(this.btn_Send_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.tb_Send);
+            this.groupBox1.Controls.Add(this.btn_Send);
+            this.groupBox1.Location = new System.Drawing.Point(92, 82);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(93, 79);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Send";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lb_Received);
+            this.groupBox2.Location = new System.Drawing.Point(326, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(157, 156);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Received";
+            // 
+            // lb_Received
+            // 
+            this.lb_Received.FormattingEnabled = true;
+            this.lb_Received.Location = new System.Drawing.Point(6, 19);
+            this.lb_Received.Name = "lb_Received";
+            this.lb_Received.Size = new System.Drawing.Size(145, 134);
+            this.lb_Received.TabIndex = 7;
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.BackColor = System.Drawing.SystemColors.Control;
+            this.trackBar1.LargeChange = 30;
+            this.trackBar1.Location = new System.Drawing.Point(22, 23);
+            this.trackBar1.Maximum = 180;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(227, 45);
+            this.trackBar1.SmallChange = 15;
+            this.trackBar1.TabIndex = 7;
+            this.trackBar1.TickFrequency = 30;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // track_label
+            // 
+            this.track_label.AutoSize = true;
+            this.track_label.Location = new System.Drawing.Point(255, 28);
+            this.track_label.Name = "track_label";
+            this.track_label.Size = new System.Drawing.Size(34, 13);
+            this.track_label.TabIndex = 8;
+            this.track_label.Text = "Angle";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 55);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(224, 55);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(25, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "180";
+            // 
+            // btn_Temizle
+            // 
+            this.btn_Temizle.Location = new System.Drawing.Point(358, 171);
+            this.btn_Temizle.Name = "btn_Temizle";
+            this.btn_Temizle.Size = new System.Drawing.Size(88, 23);
+            this.btn_Temizle.TabIndex = 11;
+            this.btn_Temizle.Text = "Clear";
+            this.btn_Temizle.UseVisualStyleBackColor = true;
+            this.btn_Temizle.Click += new System.EventHandler(this.btn_Temizle_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.cb_Ports);
+            this.groupBox3.Controls.Add(this.cb_BRates);
+            this.groupBox3.Controls.Add(this.progressBar1);
+            this.groupBox3.Controls.Add(this.label1);
+            this.groupBox3.Controls.Add(this.label2);
+            this.groupBox3.Controls.Add(this.btn_Open);
+            this.groupBox3.Controls.Add(this.btn_Close);
+            this.groupBox3.Location = new System.Drawing.Point(12, 1);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(252, 130);
+            this.groupBox3.TabIndex = 12;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Settings";
+            // 
+            // gB_pos
+            // 
+            this.gB_pos.Controls.Add(this.label4);
+            this.gB_pos.Controls.Add(this.label3);
+            this.gB_pos.Controls.Add(this.trackBar1);
+            this.gB_pos.Controls.Add(this.groupBox1);
+            this.gB_pos.Controls.Add(this.track_label);
+            this.gB_pos.Location = new System.Drawing.Point(12, 181);
+            this.gB_pos.Name = "gB_pos";
+            this.gB_pos.Size = new System.Drawing.Size(298, 165);
+            this.gB_pos.TabIndex = 16;
+            this.gB_pos.TabStop = false;
+            this.gB_pos.Text = "Position";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(12, 137);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(47, 17);
+            this.radioButton1.TabIndex = 17;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "POT";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(11, 157);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(47, 17);
+            this.radioButton2.TabIndex = 18;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "POS";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(68, 137);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(66, 37);
+            this.button1.TabIndex = 19;
+            this.button1.Text = "Buton";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(533, 352);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.gB_pos);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.btn_Temizle);
+            this.Controls.Add(this.groupBox2);
+            this.Name = "Form1";
+            this.Text = "Servo Motor Controller";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.gB_pos.ResumeLayout(false);
+            this.gB_pos.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cb_Ports;
+        private System.Windows.Forms.ComboBox cb_BRates;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_Open;
+        private System.Windows.Forms.Button btn_Close;
+        private System.Windows.Forms.TextBox tb_Send;
+        private System.Windows.Forms.Button btn_Send;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.ListBox lb_Received;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label track_label;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btn_Temizle;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox gB_pos;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports; // Eklenmek zorunda.
+using System.Threading;
+
+namespace Test_SerialPort
+{
+    public partial class Form1 : Form
+    {
+        byte[] data = new byte[4];
+        int checksum;
+        //0.byte ulaşacak ID
+        //1. byte Data tipi 0xA ise servoyu tam tur dönder. 0xB ise bir sonraki byte açı değeri
+        //son byte checksum
+        public Form1()
+        {
+            InitializeComponent();
+
+            GetPortNames(); // Her başlangıçta eklesin.
+            GetBaudRates();
+            gB_pos.Enabled = false;
+        }
+        void GetPortNames() // available ports
+        {
+            string[] ports = SerialPort.GetPortNames();
+            cb_Ports.Items.AddRange(ports);
+        }
+
+        void GetBaudRates()
+        {
+            cb_BRates.Items.Add("9600");
+            cb_BRates.Items.Add("14400");
+            cb_BRates.Items.Add("19200");
+            cb_BRates.Items.Add("38400");
+            cb_BRates.Items.Add("115200");
+        }
+
+        private void btn_Open_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if(cb_Ports.Text=="" || cb_BRates.Text=="")
+                {
+                    MessageBox.Show("Gerekli seçimler yapılmalıdır.");
+                }
+                else
+                {
+                    serialPort1.PortName = cb_Ports.Text;
+                    serialPort1.BaudRate = int.Parse(cb_BRates.Text);
+                    serialPort1.Open();
+                    progressBar1.Value = 100;
+                    MessageBox.Show("Bağlantı başarılı bir şekilde kuruldu.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btn_Close_Click(object sender, EventArgs e)
+        {
+            if(serialPort1.IsOpen)
+            {
+                serialPort1.Close();
+                progressBar1.Value = 0;
+                MessageBox.Show("Bağlantı başarılı bir şekilde sonlandırıldı.");
+            }
+            else
+            {
+                MessageBox.Show("Kapatılacak bir bağlantı yok.");
+            }
+        }
+
+        private void btn_Send_Click(object sender, EventArgs e)
+        {
+            ServoKomut(0x0B, Convert.ToByte(tb_Send.Text));
+            trackBar1.Value = int.Parse(tb_Send.Text);
+        }
+
+        byte[] datam;
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            if (!(serialPort1.IsOpen))
+            {
+                serialPort1.Open();
+            }
+            Thread.Sleep(10);
+            datam = new byte[serialPort1.BytesToRead];
+            serialPort1.Read(datam, 0, serialPort1.BytesToRead);
+            Invoke(new EventHandler(DisplayText));
+
+        }
+        private void DisplayText(object sender, EventArgs e)
+        {
+            string mystring = Encoding.Default.GetString(datam);
+            lb_Received.Items.Add(mystring);
+            lb_Received.SelectedIndex = lb_Received.Items.Count - 1;
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            ServoKomut(0x0B,Convert.ToByte(trackBar1.Value));
+        }
+
+        private void btn_Temizle_Click(object sender, EventArgs e)
+        {
+            lb_Received.Items.Clear();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)   // Pot Kontrol
+            {
+                gB_pos.Enabled = false;
+                ServoKomut(0x0A, 0);
+            }
+            else if(radioButton2.Checked)
+            {
+                gB_pos.Enabled = true;
+                ServoKomut(0x0B, Convert.ToByte(trackBar1.Value));
+            }
+        }
+
+        void ServoKomut(byte konum, byte aci)
+        {
+            data[0] = 0x01; // stm32f3
+            data[1] = konum; ;
+            data[2] = aci;
+            for (int i = 0; i < 3; i++)
+            {
+                checksum = checksum ^ data[i];
+            }
+            data[3] = Convert.ToByte(checksum);
+            serialPort1.Write(data, 0, data.Length);
+            checksum = 0;
+        }
+    }
+}
