db660f85c6893bcda6e56c35a564fec4392c5927
Yasin Cetin <yasincetin1@gmail.com>
2016-10-25 09:22:39 +0300

    Update ADC.h

--- ADC.h
+++ ADC.h
@@ -1,3 +1,3 @@
 
-uint16_t Read_ADC1(void); // return 12-bit ADC1 conversion value
+uint16_t Read_ADC1(void);      // return 12-bit ADC1 conversion value
 void ADC1_Init(void); 		// ADC1_IN1(PA0) Init and Start Conversion
